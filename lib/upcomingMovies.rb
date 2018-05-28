b8b19356a5b2314ad6e6fddb3c0fe1ac9b6dfe5c
Grace S <gracelearnsforever@gmail.com>
2018-05-28 15:05:32 -0700

    Changed order to include person and actor so that the Movie Class would be able to access the Actor and Person class information.

--- lib/upcomingMovies.rb
+++ lib/upcomingMovies.rb
@@ -1,6 +1,6 @@
 require_relative "./upcomingMovies/version"
-require_relative "./upcomingMovies/movie"
 require_relative "./upcomingMovies/person"
 require_relative "./upcomingMovies/persons/actor"
+require_relative "./upcomingMovies/movie"
 require_relative "./upcomingMovies/scrape"
 require_relative "./upcomingMovies/cli"
