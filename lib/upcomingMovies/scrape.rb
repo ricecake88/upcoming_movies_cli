e568825d62ba861aa5721855529f8947abcd9297
Grace S <gracelearnsforever@gmail.com>
2018-05-27 21:07:40 -0700

    Adding stub information to test adding Actor intances to Movie instance

--- lib/upcomingMovies/scrape.rb
+++ lib/upcomingMovies/scrape.rb
@@ -5,6 +5,11 @@ class UpcomingMovies::Scraper
     def initialize
         puts "test"
     end
+
+    @@actors = ["Tom Hanks", "Tom Cruise", "Nicole Kidman", "Rachel McAdams"]
+    # first version is to scrape coming soon information
+    # second version is to take information from coming soon and
+    # get info from movie profile page
     def scrape_upcoming(url)
         #root_imdb = "https://www.imdb.com/movies-coming-soon/"
         #imdb_html = open(root_imdb + "2018-06/?ref_=cs_dt_pv")
@@ -12,6 +17,7 @@ class UpcomingMovies::Scraper
         imdbDoc = Nokogiri::HTML(imdb_url)
         #imdbDoc = Nokogiri::HTML(imdb_html)
         titles = imdbDoc.css("div.list.detail h4")
+        actors = imdbDoc.xpath("//div[@class='txt-block']/span[@itemprop='actors']/a/text()")
 
         releaseDate = nil
         month = ""
@@ -19,6 +25,8 @@ class UpcomingMovies::Scraper
         titles.each do |node|
             if node.attributes["class"] == nil
                 movie = "#{node.text}"
+                profile_url = node.children[0].attributes["href"].value
+                
             elsif node.attributes["class"].value == "li_group"
                 releaseDate = "#{node.text}"
                 dateInfo = releaseDate.split(" ")
@@ -26,7 +34,11 @@ class UpcomingMovies::Scraper
                 date = dateInfo[1].gsub(/\u00A0/, "")
             end
           if releaseDate != nil && movie != nil 
-            UpcomingMovies::Movie.new({:name=>movie, :month=>month, :date=>date, :year=>"2018" })
+            m = UpcomingMovies::Movie.new({:name=>movie, :month=>month, :date=>date, :year=>"2018" })
+            @@actors.each do |name|
+                m.add_actor(name)
+            end
+            break
           end
         end
         
@@ -35,9 +47,9 @@ class UpcomingMovies::Scraper
         end        
     end
 
+    # add actor information to actor
     def scrape_profile(url)
-        actor1 = Actor.create_from_name("Tom Hanks")
-        actor2 = Actor.create_from_name("Meg Ryan")
+        
     end
 
     def scrape_distributors(url)
