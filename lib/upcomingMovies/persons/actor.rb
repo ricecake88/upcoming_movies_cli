24005901c4c0ad169560f26fa58e8343bd0ba35f
Grace S <gracelearnsforever@gmail.com>
2018-05-28 15:16:10 -0700

    Fixed create_by_name and find_or_create_by_name to be class methods, not instance methods

--- lib/upcomingMovies/persons/actor.rb
+++ lib/upcomingMovies/persons/actor.rb
@@ -13,13 +13,13 @@ module Persons
             @@all.detect {|actor| actor.name == name}
           end
 
-        def create_by_name(name)
+        def self.create_by_name(name)
             actor = self.new(name)
             @@all << actor
             actor
         end
 
-        def find_or_create_by_name(name)
+        def self.find_or_create_by_name(name)
             find_by_name(name) || create_by_name(name)
         end
     end
