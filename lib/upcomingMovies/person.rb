61b4a9b15238e68d7ac430250c3fe722e1759c86
Grace S <vocalise@gmail.com>
2018-05-30 15:30:01 -0700

    Removed movies array and used instance_variable_set to add instances of movies to @movies array

--- lib/upcomingMovies/person.rb
+++ lib/upcomingMovies/person.rb
@@ -1,6 +1,11 @@
 class UpcomingMovies::Person
     attr_accessor :name, :bio, :movies
     def initialize(personAttributes)
-        personAttributes.each{|key, value| self.send(("#{key}="), value)}
+        personAttributes.each do |key, value|
+        #    if key != :movies
+                self.send(("#{key}="), value)
+         #   end
+        end
+       # @movies = []
     end
 end
